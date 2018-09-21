2837853a069667254dead11631c2bd7f0fa8d126
Ender İMEN <enderimen@hotmail.com>
2018-09-21 18:36:36 +0300

    create a new module.

--- app.js
+++ app.js
@@ -3,24 +3,11 @@ var file = require('fs');
 var path = require('path');
 var express = require('express');
 var app = express();
+var ctrlElectronic = require('./customizeModules');
 
 app.use('/public', express.static(path.join(__dirname, 'public'))); // kullanıcıya erişim izni verdiğimiz dosyalar. (css vs.)
 
-var indexController = (req, res) => {
-     
-    res.sendfile(path.join(__dirname, 'login.html')); // ilk parametre ile projemizin bulund. klas. ile login.html i birleştiriyor. 
-    
-    console.log(req.url);    
-};
-
-var homeController = (req, res) => {
-    
-    res.sendfile(path.join(__dirname, 'index.html'));
-    
-        console.log(req.url);
-};
-
-app.get('/', indexController);  
-app.get('/home', homeController);
+app.get('/home', ctrlElectronic.index);  
+app.get('/computer', ctrlElectronic.computer);
 
 app.listen(8000);
